cf902c28575d2f41f6a5672fe991b453699bdcf3
Gian Pollito <1644218@senati.pe>
2025-10-22 21:03:55 -0500

    feat:Enviando documentacion de swagger, de nuevo

--- src/backend/swagger.js
+++ src/backend/swagger.js
@@ -1,18 +1,20 @@
-
-
 import swaggerJsdoc from "swagger-jsdoc";
+
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
-    info: {
-      title: "Mi API REST",
-      version: "1.0.0",
+    info: { 
+        title: "API REST", 
+        version: "1.0.0",
+        description:"Documentacion de API REST con Express, Prisma y PostgreSQL",
+        contact:{
+            email:"1644218@senati.pe"
+        }
     },
-    description: "Documentacion de API REST con Express, Prisma y PostgreSQL",
-    contac: {
-      email: "16644218@senati.pe",
-    },
-    servers: [{ url: "http://localhost:3000" }],
+    servers: [{ 
+        url: "http://localhost:3000",
+        description:"Servidor de Desarrollo" 
+    }],
   },
   apis: ["./routes/*.js"],
 };
