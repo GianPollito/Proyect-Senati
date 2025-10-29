9d4f60e9c98be22ffd0112aca3ca17a3fe09d6ce
Gian Pollito <1644218@senati.pe>
2025-10-29 11:10:01 -0500

    feat:Actualizacion de mi repositorio nuevo

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
