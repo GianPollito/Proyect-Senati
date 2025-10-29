26033f4d1e9a328bdfda539d240e9117ddf36256
Gian Pollito <1644218@senati.pe>
2025-10-29 11:00:29 -0500

    feat:Actualizcion de mi proyecto de nuevo hoy

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div className="min-h-screen bg-gray-100 p-9">
+            <h1 className="text-2x1 font-bold mb-4">Bienvenido a mi pagina</h1>
+            </div>
+    );
+}
+
+export default Home;
