afee7963811ffb18c36bb19205bdf9d2a86c97f0
Gian Pollito <1644218@senati.pe>
2025-11-05 11:25:26 -0500

    feat:Enviando mejora de LoginSuccess

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import LoginSuccess from "./pages/LoginSuccess";
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/login-success" element={<LoginSuccess />} />
       </Routes>
