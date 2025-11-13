651aee5d86d3cf82a2d3e6cf2c0ddbf5ce5c960a
Gian Pollito <1644218@senati.pe>
2025-11-13 17:22:37 -0500

    feat:Actualizacion de frontend

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -5,16 +5,22 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      boxShadow: {
-        // --- NUEVAS SOMBRAS NEÓN ---
-        'neon-border': '0 0 10px rgba(255, 0, 0, 1), 0 0 1px rgba(255, 0, 0, 1)',
-        'neon-inner': 'inset 0 0 80px 30px rgba(255, 0, 0, 1)',
-      },
-      colors: {
-        'neon-cyan': '#ff0000ff', // Definimos el color para usarlo en el borde de CSS
-      }
-    },
-  },
+    extend: {
+      boxShadow: {
+        // --- NUEVAS SOMBRAS NEÓN ---
+        'neon-border': '0 0 10px rgba(255, 0, 0, 1), 0 0 1px rgba(255, 0, 0, 1)',
+        'neon-inner': 'inset 0 0 80px 30px rgba(255, 0, 0, 1)',
+      },
+      colors: {
+        'neon': {
+            // Color principal de tu neón (rojo brillante)
+            DEFAULT: '#FF0000', 
+            // Variante oscura para el efecto hover (Rojo Borgoña/Oscuro)
+            'dark': '#8B0000', // Un tono más oscuro
+        },
+        'neon-cyan': '#ff0000ff', // Mantienes este por si lo usas en otros sitios
+      }
+    },
+  },
   plugins: [],
 }
